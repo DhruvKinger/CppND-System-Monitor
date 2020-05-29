b0fb622c0428a9726d4755242ea7bef15e57f55d
Dhruv Kinger <cse1785@outlook.com>
2020-05-29 15:36:54 +0530

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -2,10 +2,24 @@
 
 #include "format.h"
 
-using std::string;
+#define HOUR 3600
+#define MIN 60
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+using std::string;
+using std::to_string;
+
+
+string Format::ElapsedTime(long seconds) {
+  int hour, minute, second;
+  string hh, mm, ss;
+  
+  hour = seconds / HOUR;
+  second = seconds % HOUR;
+  minute = second / MIN;
+  second %= MIN;
+
+  hh = (hour < 10) ? ("0" + to_string(hour)) : to_string(hour);
+  mm = (minute < 10) ? ("0" + to_string(minute)) : to_string(minute);
+  ss = (second < 10) ? ("0" + to_string(second)) : to_string(second);
+  return hh + ":" + mm + ":" + ss;
+}
